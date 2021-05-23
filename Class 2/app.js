521bf3511e49b3e82705b26c57b27a009a151a38
unknown <sumbulrafeeq@gmail.com>
2021-05-23 13:06:55 +0500

    Implementation of JS variables and functions

---
+++ Class 2/app.js
@@ -0,0 +1,14 @@
+// const name ="farah";
+// name="sarah";
+// console.log(name);
+
+function printVariable(name)
+{
+    let name1 = name;
+    console.log(name1);
+}
+
+// printVariable("Sumbul");
+// printVariable("Rafiq");
+// printVariable("Kath");
+
